03502278ed4c926375a9d1ebb72db92824018ad0
Taki Uddin <takiuddin93@gmail.com>
2021-01-13 03:35:26 +0600

    first commit

---
+++ lib/pages/post_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:TestProject/pages/home.dart';
+import 'package:TestProject/widgets/header.dart';
+import 'package:TestProject/widgets/post.dart';
+import 'package:TestProject/widgets/progress.dart';
+
+class PostScreen extends StatelessWidget {
+  final String userId;
+  final String postId;
+
+  PostScreen({this.userId, this.postId});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: postsRef
+          .document(userId)
+          .collection('userPosts')
+          .document(postId)
+          .get(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return circularProgress();
+        }
+        Post post = Post.fromDocument(snapshot.data);
+        return Center(
+          child: Scaffold(
+            appBar: header(context, titleText: post.description),
+            body: ListView(
+              children: <Widget>[
+                Container(
+                  child: post,
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
